ab2d66d657fefe607717f6b5a8f34ea30828e8a1
Brian Weasner <weasnerb@gmail.com>
2018-09-30 22:22:16 -0500

    Config can now be migrated during different versions
    This is functionality that will eventually be used for managing multiple bridges. For Example: Selecting default bridges from a list of previously connected.

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = function (program) {
   const path = require('path');
   const fs = require('fs');
   const hue = require('node-hue-api');
+  const pkg = require('./package.json');
 
   const configPath = path.join(os.homedir(), '.hue');
 
@@ -16,6 +17,10 @@ module.exports = function (program) {
     constructor() {
       this._config = {};
       this._loadConfig();
+      // After loading the current config, see if we need to migrate it to newer versions
+      if (!this._config.version || this._config.version !== pkg.version) {
+        this._migrateConfig(this._config.version);
+      }
     }
 
     /**
@@ -53,18 +58,18 @@ module.exports = function (program) {
      */
     _userExists() {
       new hue.api(this.bridge, this.user)
-      .registeredUsers()
-      .then(data => {
-        let exists =  data.devices.filter(u => u.username === this.user).length == 1;
-        if (!exists) {
-          delete this._config.user
-          this._saveConfig();
+        .registeredUsers()
+        .then(data => {
+          let exists = data.devices.filter(u => u.username === this.user).length == 1;
+          if (!exists) {
+            delete this._config.user
+            this._saveConfig();
 
-          let messages = ['User is no longer authenticated', 'Please run setup to re-authenticate']
-          return program.util.errorMessage(messages);
-        }
-      })
-      .fail(err => program.util.errorMessage('Unable to check if user exists', err));
+            let messages = ['User is no longer authenticated', 'Please run setup to re-authenticate']
+            return program.util.errorMessage(messages);
+          }
+        })
+        .fail(err => program.util.errorMessage('Unable to check if user exists', err));
     }
 
     /**
@@ -79,12 +84,75 @@ module.exports = function (program) {
         // Do nothing
       }
     }
-    
+
     /**
      * Save the config
      */
     _saveConfig() {
       fs.writeFileSync(configPath, JSON.stringify(this._config))
+    }
+
+
+    /**
+     * Migrate current config structure to new config structure
+     */
+    _migrateConfig() {
+      // Make sure current config version exists, if not set to lowest version
+      let currentConfigVersion = !this._config.version ? '0.0.0' : this._config.version;
+
+      // Migration Dir
+      const loadPath = path.join(path.dirname(__filename), 'migrations/config');
+
+      if (fs.existsSync(loadPath)) {
+        // Loop though migration files
+        fs.readdirSync(loadPath)
+          .filter((filename) => {
+            // Only get files with proper naming and where semver of file is newer than current config's version
+            return (/\d\.\d\.\d\.js$/.test(filename) && this._semVersionEquals(currentConfigVersion, this._getFilenameWithoutExtension(filename)) == -1);
+          })
+          .sort((filenameA, filenameB) => this._semVersionEquals(this._getFilenameWithoutExtension(filenameA), this._getFilenameWithoutExtension(filenameB)))
+          .forEach((filename) => {
+            require(path.join(loadPath, filename))(this._config);
+          });
+      }
+
+      this._config.version = pkg.version;
+      this._saveConfig();
+    }
+
+    /**
+     * Get a semver from a se
+     * @param {string} filename 
+     */
+    _getFilenameWithoutExtension(filename) {
+      if (typeof filename !== 'string') {
+        new Error('Filename is not a string');
+      }
+      return path.basename(filename, path.extname(filename));
+    }
+
+    /**
+     * Check to see if a semver is greater than, equal to, or less than the other
+     * @param {string} semVerOne 
+     * @param {string} semVerTwo 
+     */
+    _semVersionEquals(semVerOne, semVerTwo) {
+      if (typeof semVerOne !== 'string' || typeof semVerTwo !== 'string') {
+        new Error('semvers must be strings')
+      }
+      let versionA = semVerOne.split('.');
+      let versionB = semVerTwo.split('.');
+      if (!(Array.isArray(versionA) && Array.isArray(versionB) && versionA.length === versionB.length)) {
+        new Error('semvers must be properly formatted')
+      }
+      // npm Symantic Versioning only has 3 numbers
+      for (let i = 0; i < 3; i++) {
+        let order = versionA[i] - versionB[i]
+        if (order !== 0) {
+          return (order > 0 ? 1 : -1)
+        }
+      }
+      return 0;
     }
 
   }
